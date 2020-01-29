7c189bda6eb8635bd9456a16ddecf18f4e562a67
Ray <ray@Rays-MacBook-Pro.local>
2020-01-29 17:38:37 +0200

    Implemented sorting

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 //Normālos gadījumos šeit būtu $getJSON, kas datus saņemtu caur API, konkrēti šim uzdevumam izveidoju vienkāršu data list
 var data = [
   {
+    "id": 0,
     "pic": "https://images.unsplash.com/photo-1580200346290-a0bd91f0debd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     "title": "Green trees",
     "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -9,6 +10,7 @@ var data = [
     "backgroundColor": "#32a852",
   },
   {
+    "id": 1,
     "pic": "https://images.unsplash.com/photo-1580231679388-43b7bf42f7c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     "title": "Sahara",
     "text": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
@@ -17,6 +19,7 @@ var data = [
     "backgroundColor": "#b594e3",
   },
   {
+    "id": 2,
     "pic": "https://images.unsplash.com/photo-1580237995396-a21928e71e63?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
     "title": "Blue sea",
     "text": " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -25,24 +28,35 @@ var data = [
     "backgroundColor": "#fffbd6",
   }
 ];
+var reverse = [1, -1];  // Array to toggle reverse (Initial sort is from end to start)
 
-$.each(data, function (key, val) {
-  createListItems(val.pic, val.title, val.text, val.backgroundColor, val.fontSize, val.textColor);
+$( document ).ready(function() {
+  loadItems();
 });
 
-function createListItems(pic, title, text, color, fontSize, textColor) {
+function loadItems() {
+  $('.main').empty();
+  $.each(data, function (key, val) {
+    createListItems(val.id, val.pic, val.title, val.text, val.backgroundColor, val.fontSize, val.textColor);
+  });
+}
+
+function createListItems(id, pic, title, text, color, fontSize, textColor) {
   item = $('.main').append(
     $('<div>').addClass('section').append(
       $('<div>').addClass('container').css({"background-color": color}).append(
-        $('<a>').attr('onclick', "remove(this)").text("X"), $('<img>').attr('src', pic), $('<h1>').append(title), $('<p>').append(text).css({"font-size": fontSize, "color": textColor})
+        $('<a>').attr({onclick: "remove(this)", id: id}).text("X"), $('<img>').attr('src', pic), $('<h1>').append(title), $('<p>').append(text).css({"font-size": fontSize, "color": textColor})
       )
     )
   );
 }
 
-// TODO: Implement removing the item from the data list object in use
 function remove(e) {
-  e.closest('div.section').remove();
+  var toDelete = e.id;
+  data = data.filter(function( obj ) {
+    return obj.id != toDelete;
+  }, loadItems());
+
 }
 
 // http://css-tricks.com/snippets/jquery/serialize-form-to-json/
@@ -62,11 +76,28 @@ $.fn.serializeObject = function() {
   return o;
 };
 
-// TODO: Implement adding the submitted data to the data list object in use
 $(function() {
   $('form').on('submit', function(e) {
     e.preventDefault();
     var formData = $(this).serializeObject();
-    createListItems(formData.pic, formData.title, formData.text, formData.backgroundColor, formData.fontSize, formData.textColor);
+    data.push(
+      {id: data.length ,pic: formData.pic, title: formData.title, text: formData.text, fontSize: formData.fontSize, textColor: formData.textColor, backgroundColor: formData.backgroundColor}
+    );
+    loadItems();
   });
 });
+
+function sortData(sortBy) {
+  data.sort(dataSorter(sortBy));
+  loadItems();
+}
+
+function dataSorter(field) {
+  var key = function(x) {
+    return x[field]
+  };
+  reverse.unshift(reverse.pop())
+  return function(a, b) {
+    return a = key(a), b = key(b), parseInt(reverse,10) * ((a > b) - (b > a));
+  }
+}
